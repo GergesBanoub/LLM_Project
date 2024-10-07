a06d1999ed09980ba17d6e0b86f0d46cdc8bdaca
gerges banoub <gergesbanoub@yahoo.com>
2024-10-07 16:51:23 +0300

    commit

--- helpers/config.py
+++ helpers/config.py
@@ -1,1 +1,15 @@
-from pydantic-settings  import basese 
+from pydantic_settings  import BaseSettings ,SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    APP_VERSION: str
+    OPENAI_API_KEY: str
+    
+    class Config:  # Fix the typo (use Config with uppercase C)
+        env_file = "/home/mini_rag_app/LLM_Project/.env"  # Path to your .env file
+
+def get_settings():
+    return Settings()
+
+ 
--- routes/data.py
+++ routes/data.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI,APIRouter,Depends
 from helpers.config import get_settings  
 
-base = APIRouter()
-@base.get("/")
+data_router = APIRouter(
+ prefix="/api/v1/data",
+ tags=["api_v1","data"],
+
+)
+@data_router.post("/upload")
 def welcome (app_settings=Depends(get_settings)):
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
